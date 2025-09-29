a2bddf6a98e85cf33b793954b4bfd0a500977564
r <e>
2025-09-29 17:02:18 +0300

    Initial Commit

---
+++ lunchBox2/lunchBox2App.swift
@@ -0,0 +1,17 @@
+//
+//  lunchBox2App.swift
+//  lunchBox2
+//
+//  Created by رحاب فهد  on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct lunchBox2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
